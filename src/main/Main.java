07190df193818324c65fd40673c70b2fe9a988a2
user <user@example.com>
2025-10-27 15:11:38 +0500

    Teacher's advices + question answers

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,18 +1,23 @@
 package main;
 
 import context.TwoFactorAuthContext;
+import strategy.AuthStrategy;
 import strategy.AuthThroughSMS;
 import strategy.AuthThroughSecretQuestion;
 
 public class Main {
     public static void main(String[] args) {
-        String username = "Rakhat0612";
+        String username1 = "Rakhat0612";
+        String username2 = "Adam1204";
+
+        AuthStrategy authThroughSMS = new AuthThroughSMS();
+        AuthStrategy authThroughSecretQuestion = new AuthThroughSecretQuestion();
 
         TwoFactorAuthContext context = new TwoFactorAuthContext();
-        context.setStrategy(new AuthThroughSMS());
-        context.login(username);
+        context.setStrategy(authThroughSMS);
+        context.login(username1);
 
-        context.setStrategy(new AuthThroughSecretQuestion());
-        context.login(username);
+        context.setStrategy(authThroughSecretQuestion);
+        context.login(username2);
     }
 }
